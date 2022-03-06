7ea6bda74279ef7fd4a1b493ff9cc35be6d17e81
mrgreentm <joaonetotfm@gmail.com>
2022-03-06 15:25:11 -0300

    fix: remove-percentage-skilss

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-  projectsAndArticles: Array<any> = [{ name: 'ISS - PROJECT', url: 'https://github.com/mrgreentm/iss-project' }
-    , { name: 'Ricky & Morty', url: 'https://github.com/mrgreentm/rick-and-morty' }, { name: 'Pegasus', url: 'https://github.com/mrgreentm/pegasus' },
-  { name: 'Game Dev', url: 'https://github.com/mrgreentm/game-dev' }, { name: 'Calorimetria - Física 2', url: 'https://github.com/mrgreentm/calorimetry' }
+  projectsAndArticles: Array<any> = [
+    { name: 'ISS - PROJECT', url: 'https://github.com/mrgreentm/iss-project' },
+    { name: 'Ricky & Morty', url: 'https://github.com/mrgreentm/rick-and-morty' },
+    { name: 'Pegasus', url: 'https://github.com/mrgreentm/pegasus' },
+    { name: 'Game Dev', url: 'https://github.com/mrgreentm/game-dev' },
+    { name: 'Calorimetria - Física 2', url: 'https://github.com/mrgreentm/calorimetry' }
   ]
   constructor() { }
 
--- src/app/stacks/stacks.component.ts
+++ src/app/stacks/stacks.component.ts
@@ -4,18 +4,17 @@ import {ProgressSpinnerMode} from '@angular/material/progress-spinner';
 
 export interface Stacks {
   name: string;
-  weight: number;
   symbol: string;
 }
 
 const ELEMENT_DATA: Stacks[] = [
-  { name: 'Angular', weight: 80, symbol: '/assets/angular.svg' },
-  { name: 'Angular Material', weight: 90, symbol: '/assets/material.svg' },
-  { name: 'Web Components', weight: 75, symbol: '/assets/components.svg' },
-  { name: 'Forms', weight: 60, symbol: '/assets/forms.svg' },
-  { name: 'JavaScript', weight: 70, symbol: '/assets/js.png' },
-  { name: 'HTML5', weight: 90, symbol: '/assets/html-5.png' },
-  { name: 'CSS', weight: 85, symbol: '/assets/css-3.png' },
+  { name: 'Angular', symbol: '/assets/angular.svg' },
+  { name: 'Angular Material', symbol: '/assets/material.svg' },
+  { name: 'Web Components', symbol: '/assets/components.svg' },
+  { name: 'Forms', symbol: '/assets/forms.svg' },
+  { name: 'JavaScript', symbol: '/assets/js.png' },
+  { name: 'HTML5', symbol: '/assets/html-5.png' },
+  { name: 'CSS', symbol: '/assets/css-3.png' },
 ];
 
 
@@ -30,7 +29,7 @@ export class StacksComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  displayedColumns: string[] = ['name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['name', 'symbol'];
   dataSource = ELEMENT_DATA;
   color: ThemePalette = 'primary';
 
